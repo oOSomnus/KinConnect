fa42327c6e3696bfd43c5adf0db3117b6d072ea7
Yihang Zhu <yihangz@umich.edu>
2025-09-26 21:49:51 -0400

    re-initialize project

---
+++ server/src/main/java/com/github/KinConnect/KinConnectApplication.java
@@ -0,0 +1,13 @@
+package com.github.KinConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinConnectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/github/KinConnect/KinConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.KinConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
